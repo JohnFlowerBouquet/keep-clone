cc9ef39debbab67060c51c82acd7276e65965e20
John <john.flower.bouquet@gmail.com>
2019-02-28 22:16:21 +0100

    Updates

---
+++ precache-manifest.a077f88c115728f6783a10f0f581c6b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c5a6f5a82af2d392a46",
+    "url": "/keep-clone/static/js/runtime~main.7c5a6f5a.js"
+  },
+  {
+    "revision": "dc9cc8ab3855dac2660f",
+    "url": "/keep-clone/static/js/main.dc9cc8ab.chunk.js"
+  },
+  {
+    "revision": "4c95b3028ba5e51267cf",
+    "url": "/keep-clone/static/js/2.4c95b302.chunk.js"
+  },
+  {
+    "revision": "b63477ecd1dd538c17c3adb9f10b9e2c",
+    "url": "/keep-clone/index.html"
+  }
+];
