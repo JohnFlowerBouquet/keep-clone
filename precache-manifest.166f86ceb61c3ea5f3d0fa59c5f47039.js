975b57172301c5381d2f5b4567713aa539f0d355
John <john.flower.bouquet@gmail.com>
2019-05-13 23:02:43 +0200

    Updates

---
+++ precache-manifest.166f86ceb61c3ea5f3d0fa59c5f47039.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025935891826abe0bb59e67f476b7e80",
+    "url": "/keep-clone/index.html"
+  },
+  {
+    "revision": "590f2cf06162af004053",
+    "url": "/keep-clone/static/js/2.4b3f0412.chunk.js"
+  },
+  {
+    "revision": "c15bf842a0c964ccdfa0",
+    "url": "/keep-clone/static/js/main.51c5a72b.chunk.js"
+  },
+  {
+    "revision": "bcb6070b8627489aa57d",
+    "url": "/keep-clone/static/js/runtime~main.dc228a56.js"
+  }
+]);
